4ce845523ac20243817f7241b6d854a360f4384a
Omusubi0123 <Vermouth77rseidl@gmail.com>
2024-12-05 00:43:21 +0900

    feat: 全カテゴリーにて質問取得

---
+++ scraping/g_category_info.py
@@ -0,0 +1,44 @@
+base_url = {
+    "new": "https://oshiete.goo.ne.jp/articles/qa/{category_id}/?sort=2&pg={page}",
+    "best": "https://oshiete.goo.ne.jp/articles/qa/{category_id}/?sort=2&pg={page}&target=best",
+    "ranking": "https://oshiete.goo.ne.jp/articles/ranking/{category_id}/?pg={page}",
+    "ans_num": "https://oshiete.goo.ne.jp/articles/qa/{category_id}/?pg={page}&sort=4",
+}
+category_dict = {
+    "出会い・合コン": {
+        "category_id": 2546,
+        "page_num": {"new": 20, "best": 20, "ranking": 3, "ans_num": 40},
+    },
+    "モテる・モテたい": {
+        "category_id": 2547,
+        "page_num": {"new": 20, "best": 20, "ranking": 4, "ans_num": 40},
+    },
+    "恋愛占い・恋愛運": {
+        "category_id": 2548,
+        "page_num": {"new": 20, "best": 20, "ranking": 4, "ans_num": 40},
+    },
+    "片思い・告白": {
+        "category_id": 2549,
+        "page_num": {"new": 20, "best": 20, "ranking": 4, "ans_num": 40},
+    },
+    "カップル・彼氏・彼女": {
+        "category_id": 2550,
+        "page_num": {"new": 20, "best": 20, "ranking": 4, "ans_num": 40},
+    },
+    "デート・キス": {
+        "category_id": 2551,
+        "page_num": {"new": 20, "best": 20, "ranking": 4, "ans_num": 40},
+    },
+    "浮気・不倫（恋愛相談）": {
+        "category_id": 2552,
+        "page_num": {"new": 20, "best": 20, "ranking": 4, "ans_num": 40},
+    },
+    "失恋・別れ": {
+        "category_id": 2553,
+        "page_num": {"new": 20, "best": 20, "ranking": 4, "ans_num": 40},
+    },
+    "その他（恋愛相談）": {
+        "category_id": 2554,
+        "page_num": {"new": 20, "best": 20, "ranking": 4, "ans_num": 40},
+    },
+}
---
+++ scraping/g_category_question.py
@@ -0,0 +1,93 @@
+import os
+from typing import Literal
+from urllib.parse import urljoin
+
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+from scraping.g_category_info import base_url, category_dict
+
+# Chromeドライバーの設定
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+service = Service("/usr/local/bin/chromedriver-linux64/chromedriver")
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+
+def get_question_urls(
+    category: str,
+    base_url: str,
+    category_id: int,
+    page_num: int,
+    key: Literal["new", "best", "ranking", "ans_num"],
+    save_path: str = "data/goo_question/{category}_questions.csv",
+):
+    save_path = save_path.format(category=category)
+
+    if not os.path.exists(os.path.dirname(save_path)):
+        os.makedirs(os.path.dirname(save_path))
+        question_df = pd.DataFrame(columns=["category", "title", "url", "key", "page"])
+    else:
+        question_df = pd.read_csv(save_path)
+
+    for page in range(1, page_num + 1):
+        url = base_url.format(category_id=category_id, page=page)
+
+        # ページの読み込み
+        driver.get(url)
+
+        # ページが完全に読み込まれるまで待機
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "qalistItem"))
+        )
+
+        # ページのソースを取得
+        page_source = driver.page_source
+
+        # BeautifulSoupでHTMLを解析
+        soup = BeautifulSoup(page_source, "html.parser")
+
+        # <li class="qalistItem">要素を全て取得
+        qalist_items = soup.find_all("li", class_="qalistItem")
+
+        # URLを格納するリスト
+        urls = []
+        titles = []
+        # 各qalistItem要素から<a>タグのURLを取得
+        for item in qalist_items:
+            a_tag = item.find("a", attrs={"data-osccid": "qa"})
+            if a_tag:
+                titles.append(a_tag.text.strip())
+                urls.append(urljoin(url, a_tag["href"]))
+
+        # 取得したURLをDataFrameに格納
+        df = pd.DataFrame(
+            {
+                "category": [category] * len(urls),
+                "title": titles,
+                "url": urls,
+                "key": [key] * len(urls),
+                "page": [page] * len(urls),
+            }
+        )
+        question_df = pd.concat([question_df, df], ignore_index=True)
+    question_df.to_csv(save_path, index=False)
+
+
+# ブラウザを閉じる
+driver.quit()
+
+
+def get_category_questions():
+    for category, value in category_dict.items():
+        category_id = value["category_id"]
+        page_num = value["page_num"]
+
+        for key in page_num.keys():
+            get_question_urls(category, base_url[key], category_id, page_num[key])
