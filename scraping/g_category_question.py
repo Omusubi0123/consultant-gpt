0b3c97afd9f79bb6bd7899295fcced59caf84a56
Omusubi0123 <Vermouth77rseidl@gmail.com>
2024-12-05 09:38:36 +0900

    feat: g でのスクレイピングで失敗した個所を修正

--- scraping/g_category_question.py
+++ scraping/g_category_question.py
@@ -1,4 +1,5 @@
 import os
+from time import sleep
 from typing import Literal
 from urllib.parse import urljoin
 
@@ -22,61 +23,74 @@ def get_question_urls(
     page_num: int,
     key: Literal["new", "best", "ranking", "ans_num"],
     save_path: str = "data/goo_question/{category}_questions.csv",
+    max_retry: int = 3,
+    timeout: int = 20,
+    error_sleep: int = 5,
 ):
     save_path = save_path.format(category=category)
 
-    if not os.path.exists(os.path.dirname(save_path)):
-        os.makedirs(os.path.dirname(save_path))
-        question_df = pd.DataFrame(columns=["category", "title", "url", "key", "page"])
-    else:
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    if os.path.exists(save_path):
         question_df = pd.read_csv(save_path)
+    else:
+        question_df = pd.DataFrame(columns=["category", "title", "url", "key", "page"])
 
     for page in range(1, page_num + 1):
         print(f"{category} {key} page {page}/{page_num}")
-        try:
-            url = base_url.format(category_id=category_id, page=page)
 
-            # ページの読み込み
-            driver.get(url)
+        retry = 0
+        while retry < max_retry:
+            try:
+                url = base_url.format(category_id=category_id, page=page)
 
-            # ページが完全に読み込まれるまで待機
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "qalistItem"))
-            )
+                # ページの読み込み
+                driver.get(url)
 
-            # ページのソースを取得
-            page_source = driver.page_source
+                # ページが完全に読み込まれるまで待機
+                element_present = EC.presence_of_element_located(
+                    (By.CLASS_NAME, "qalistItem")
+                )
+                WebDriverWait(driver, timeout).until(element_present)
 
-            # BeautifulSoupでHTMLを解析
-            soup = BeautifulSoup(page_source, "html.parser")
+                # ページのソースを取得
+                page_source = driver.page_source
 
-            # <li class="qalistItem">要素を全て取得
-            qalist_items = soup.find_all("li", class_="qalistItem")
+                # BeautifulSoupでHTMLを解析
+                soup = BeautifulSoup(page_source, "html.parser")
 
-            # URLを格納するリスト
-            urls = []
-            titles = []
-            # 各qalistItem要素から<a>タグのURLを取得
-            for item in qalist_items:
-                a_tag = item.find("a", attrs={"data-osccid": "qa"})
-                if a_tag:
-                    titles.append(a_tag.text.strip())
-                    urls.append(urljoin(url, a_tag["href"]))
+                # <li class="qalistItem">要素を全て取得
+                qalist_items = soup.find_all("li", class_="qalistItem")
 
-            # 取得したURLをDataFrameに格納
-            df = pd.DataFrame(
-                {
-                    "category": [category] * len(urls),
-                    "title": titles,
-                    "url": urls,
-                    "key": [key] * len(urls),
-                    "page": [page] * len(urls),
-                }
-            )
-            question_df = pd.concat([question_df, df], ignore_index=True)
-        except Exception as e:
-            print(e)
-            
+                # URLを格納するリスト
+                urls = []
+                titles = []
+                # 各qalistItem要素から<a>タグのURLを取得
+                for item in qalist_items:
+                    a_tag = item.find(
+                        "a",
+                        attrs={"data-osccid": "qa" if key != "ranking" else "ranking"},
+                    )
+                    if a_tag:
+                        titles.append(a_tag.text.strip())
+                        urls.append(urljoin(url, a_tag["href"]))
+
+                # 取得したURLをDataFrameに格納
+                df = pd.DataFrame(
+                    {
+                        "category": [category] * len(urls),
+                        "title": titles,
+                        "url": urls,
+                        "key": [key] * len(urls),
+                        "page": [page] * len(urls),
+                    }
+                )
+                question_df = pd.concat([question_df, df], ignore_index=True)
+                break
+            except Exception as e:
+                retry += 1
+                print(e)
+                sleep(error_sleep)
+
     question_df.to_csv(save_path, index=False)
 
 
@@ -92,10 +106,14 @@ def get_category_questions(driver, category_dict, base_url):
 
 
 if __name__ == "__main__":
+    timeout = 20
+
     chrome_options = Options()
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--dns-prefetch-disable")
     service = Service("/usr/local/bin/chromedriver-linux64/chromedriver")
     driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver.timeouts.page_load = timeout * 1000
 
     get_category_questions(driver, category_dict, base_url)
 
