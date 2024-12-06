b97a8c1bb17958fb371d187dc1a0db5210159dcd
Omusubi0123 <Vermouth77rseidl@gmail.com>
2024-12-06 23:23:04 +0900

    feat: gのQ&Aを取得

--- scraping/g_question_answer.py
+++ scraping/g_question_answer.py
@@ -1,9 +1,11 @@
 import os
+import re
 from time import sleep
 from typing import Any, Literal
 from urllib.parse import urljoin
 
 import pandas as pd
+import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -11,6 +13,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from tqdm import tqdm
 
 from scraping.g_category_info import base_url, category_dict
 
@@ -34,16 +37,10 @@ def get_question_answer(
     retry = 0
     while retry < max_retry:
         try:
-            url = url
+            response = requests.get(url)
+            response.raise_for_status()
 
-            driver.get(url)
-
-            element_present = EC.presence_of_element_located((By.CLASS_NAME, "a_text"))
-            WebDriverWait(driver, timeout).until(element_present)
-
-            page_source = driver.page_source
-
-            soup = BeautifulSoup(page_source, "html.parser")
+            soup = BeautifulSoup(response.text, "html.parser")
 
             question_title = soup.find("div", class_="q_article_info").find("h1").text
             question_text = soup.find("p", class_="q_text").get_text(
@@ -61,11 +58,15 @@ def get_question_answer(
         except Exception as e:
             retry += 1
             print(e)
+            sleep(error_sleep)
     return None, None, None
 
 
-def get_category_questions(
-    driver: webdriver.Chrome, category_dict: dict[str, Any], base_url: dict[str, str]
+def get_category_question_answer(
+    driver: webdriver.Chrome,
+    category_dict: dict[str, Any],
+    csv_path: str = "data/goo_question/{}_questions.csv",
+    save_file: str = "data/goo_q_n_a/{}.jsonl",
 ):
     """9つのカテゴリそれぞれについて、質問一覧ページから質問のURLを取得し、CSVファイルに保存する
 
@@ -74,14 +75,39 @@ def get_category_questions(
         category_dict (dict): 9つのカテゴリに関する情報を格納した辞書
         base_url (dict): 質問一覧ページのURL
     """
-    for category, value in category_dict.items():
-        category_id = value["category_id"]
-        page_num = value["page_num"]
+    os.makedirs(os.path.dirname(save_file), exist_ok=True)
+    for category in category_dict.keys():
+        df = pd.read_csv(csv_path.format(category))
 
-        for key in page_num.keys():
-            get_question_urls(
-                driver, category, base_url[key], category_id, page_num[key], key
+        q_a_df = pd.DataFrame(columns=["q_id", "q_title", "q_text", "a_texts"])
+        for i, url in enumerate(tqdm(df["url"], total=len(df))):
+            question_title, question_text, answer_texts = get_question_answer(
+                driver, url
             )
+            question_id = re.search(r"/qa/(\d+)", url).group(1)
+            if question_title is not None:
+                q_a_dict = {
+                    "q_id": question_id,
+                    "q_title": question_title,
+                    "q_text": question_text,
+                    "a_texts": answer_texts,
+                }
+                q_a_df = pd.concat(
+                    [q_a_df, pd.DataFrame([q_a_dict])], ignore_index=True
+                )
+
+                if i % 100 == 0:
+                    q_a_df.to_json(
+                        save_file.format(category),
+                        orient="records",
+                        lines=True,
+                        force_ascii=False,
+                    )
+            else:
+                print(f"Failed to get question and answer from {question_id}")
+        q_a_df.to_json(
+            save_file.format(category), orient="records", lines=True, force_ascii=False
+        )
 
 
 if __name__ == "__main__":
@@ -97,6 +123,6 @@ if __name__ == "__main__":
     driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.timeouts.page_load = timeout * 1000
 
-    get_category_questions(driver, category_dict, base_url)
+    get_category_question_answer(driver, category_dict)
 
     driver.quit()
